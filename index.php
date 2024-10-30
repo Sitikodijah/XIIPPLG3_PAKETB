bce3dcfd8b0f52f7959b6db0e842bd051d6cbdd3
Siti Kodijah <144641293+Sitikodijah@users.noreply.github.com>
2024-10-30 14:31:11 +0700

    dijah

--- index.php
+++ index.php
@@ -17,9 +17,21 @@
       <div class="navbar-nav">
         
       </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
     </div>
   </div>
 </nav>
+
+<div class="container mt-3">
+  <div class="row">
+    <div class="col-md-3">
+      <div class="card">
+        <img src="" class="card-img-top" alt="">
+      </div>
+    </div>
+  </div>
+</div>
 
 <script type="text/javasript" src="asset/js/bootstrap.min.js"></script>
 </body>
