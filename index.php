f16639f2efe77b34bedacd15c2892e6b214ae997
Siti Kodijah <144641293+Sitikodijah@users.noreply.github.com>
2024-10-30 13:03:50 +0700

    dijah

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-.phj
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="widht=device-widht, initial-scale=1">
+  <title>Website Galeri Foto</title>
+  <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+</head>
+<body>
+    
+
+<script type=""></script>
+</body>
+</html>
